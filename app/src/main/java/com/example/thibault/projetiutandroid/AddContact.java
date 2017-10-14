2940f6273c52992fb076f074939a55f9ccd1f76d
Falcort <falcort.jeux@gmail.com>
2017-10-14 11:43:57 +0200

    Button ajout de photo

--- app/src/main/java/com/example/thibault/projetiutandroid/AddContact.java
+++ app/src/main/java/com/example/thibault/projetiutandroid/AddContact.java
@@ -1,12 +1,17 @@
 package com.example.thibault.projetiutandroid;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageSwitcher;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -15,6 +20,7 @@ import java.util.ArrayList;
 
 public class AddContact extends AppCompatActivity implements Serializable
 {
+    ImageSwitcher imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -89,6 +95,61 @@ public class AddContact extends AppCompatActivity implements Serializable
             intent.putExtra("listContact", contact);
             startActivity(intent);
             finish();
+        }
+    }
+
+    public void onAddPhoto(View view)
+    {
+        final CharSequence[] items = {"Prendre une photo", "Chosiir depuis la gallerie", "Annulée"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddContact.this);
+        builder.setTitle("Ajouter une photo");
+        builder.setItems(items, new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                if(i == 0)
+                {
+                    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                }
+                else if(i == 1)
+                {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+                }
+                else
+                {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent)
+    {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode)
+        {
+            case 0:
+            {
+                if(resultCode == RESULT_OK)
+                {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageview.setImageURI(selectedImage);
+                }
+                break;
+            }
+            case 1:
+                {
+                if(resultCode == RESULT_OK)
+                {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageview.setImageURI(selectedImage);
+                }
+                break;
+            }
         }
     }
 }
