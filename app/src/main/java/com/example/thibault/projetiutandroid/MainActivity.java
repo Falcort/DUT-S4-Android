7373eefa93a718912dbebe3b504171a89c5f21a8
Falcort <falcort.jeux@gmail.com>
2017-10-14 11:27:19 +0200

    Dialog de suppression

--- app/src/main/java/com/example/thibault/projetiutandroid/MainActivity.java
+++ app/src/main/java/com/example/thibault/projetiutandroid/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity implements Serializable
     ListView listView;
 
     SimpleAdapter adapter;
-    int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -58,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements Serializable
         listView = (ListView) findViewById(R.id.listViewContact);
         adapter = new SimpleAdapter(this.getBaseContext(), ArrayMap, R.layout.list_view_contact_layout, new String[] {"name", "phone", "email", "sexe"}, new int[] {R.id.titre});
         listView.setAdapter(adapter);
+
         listView.setClickable(true);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
@@ -72,14 +72,35 @@ public class MainActivity extends AppCompatActivity implements Serializable
                 toast.show();
             }
         });
-
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
         {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l)
             {
-                ArrayMap.remove(i);
-                adapter.notifyDataSetChanged();
+                final int index = i;
+                final CharSequence[] items = {"Supprimer contact", "Annulé"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Suppresion contact");
+                builder.setItems(items, new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int item)
+                    {
+                        if(items[item].equals("Supprimer contact"))
+                        {
+                            Log.d("STATE", "\nSuppresion\n");
+                            listString.remove(index);
+                            ArrayMap.remove(index);
+                            adapter.notifyDataSetChanged();
+                        }
+                        else
+                        {
+                            Log.d("STATE", "\nAnnulation\n");
+                            dialogInterface.dismiss();
+                        }
+                    }
+                });
+                builder.show();
                 return true;
             }
         });
